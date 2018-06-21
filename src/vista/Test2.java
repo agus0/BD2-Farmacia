e3a45dbc29f3407d214ff42e99e5c2512b97259a
JavierVescio <javiervescio92@gmail.com>
2018-06-21 00:09:34 -0300

    Cambios menores

--- src/vista/Test2.java
+++ src/vista/Test2.java
@@ -16,7 +16,7 @@ public class Test2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		GregorianCalendar fecha1 = new GregorianCalendar(2018,5,1);
+		GregorianCalendar fecha1 = new GregorianCalendar(2018,4,1);
 		GregorianCalendar fecha2 = new GregorianCalendar(2018,5,30);
 		
 		SucursalABM abmSucursal = new SucursalABM();
